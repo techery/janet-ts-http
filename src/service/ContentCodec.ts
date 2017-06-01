ad5de5016bebf52f618bd04a25d4cb6ecb307dc0
Sergey Zenchenko <sergeizenchenko@gmail.com>
2017-06-01 16:33:42 +0300

    Refactoring.

--- src/service/ContentCodec.ts
+++ src/service/ContentCodec.ts
@@ -1,20 +1,24 @@
 export interface Serializer {
-    serialize(content: any): any;
+  serialize(content: any): any;
 }
 
 export interface Deserializer {
-    deserialize(content: any): any;
+  deserialize(content: any): any;
 }
 
 export class JSONSerializer implements Serializer {
 
-    serialize(content: any): any {
-        return JSON.stringify(content);
-    }
+  serialize(content: any): any {
+    return JSON.stringify(content);
+  }
 }
 
 export class JSONDeserializer implements Deserializer {
-    deserialize(content: any): any {
-        return JSON.parse(content);
+  deserialize(content: any): any {
+    try {
+      return JSON.parse(content);
+    } catch (e) {
+      return null;
     }
+  }
 }
